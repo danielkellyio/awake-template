c7f055e29417a2ed40cdeac6a3b9538485b86f5f
Daniel Kelly <danielkelly@randallreilly.com>
2019-07-26 12:55:10 -0500

    updated opti-image and styled pre tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,48 @@ import NewsLetterForm from '~/components/NewsLetterForm'
 export default {
   components: { SiteNav, SiteFooter, NewsLetterForm },
   transition: 'slide-fade',
+  head() {
+    return {
+      title: `${this.$store.state.title} | ${this.$siteConfig.siteName}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$store.state.subtitle
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.$store.state.subtitle
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.$store.state.title
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: this.$store.state.featureImage
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: this.url
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: `summary_large_image`
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: this.$siteConfig.siteName
+        }
+      ]
+    }
+  },
   data() {
     return {
       items: [
--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -55,48 +55,6 @@ export default {
     MainSection,
     DisqusComments
   },
-  head() {
-    return {
-      title: `${this.$store.state.title} | ${this.$siteConfig.siteName}`,
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$store.state.subtitle
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: this.$store.state.subtitle
-        },
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: this.$store.state.title
-        },
-        {
-          hid: 'og:image',
-          property: 'og:image',
-          content: this.$store.state.featureImage
-        },
-        {
-          hid: 'og:url',
-          property: 'og:url',
-          content: this.url
-        },
-        {
-          hid: 'twitter:card',
-          name: 'twitter:card',
-          content: `summary_large_image`
-        },
-        {
-          hid: 'og:site_name',
-          name: 'og:site_name',
-          content: process.env.npm_package_name
-        }
-      ]
-    }
-  },
   computed: {
     ...mapState([
       'title',
