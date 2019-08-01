9f13a59c7d112e27337aae132bd2328f1e16e340
Daniel Kelly <danielkelly@randallreilly.com>
2019-07-31 21:57:10 -0500

    fixed categoies link and made with love in footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -2,6 +2,9 @@
   <footer class="site-footer">
     {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - All
     Rights Reserved
+    <br />
+    Made with <span style="color: #e25555;">&#9829;</span> with Nuxt.js and
+    Netlify
   </footer>
 </template>
 
