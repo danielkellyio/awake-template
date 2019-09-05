28b25a20e2443d079845a8b67e1b77cfa89096a0
Bob Walsh <bob.walsh@47hats.com>
2019-09-05 12:28:34 -0700

    replaced default font with Ovo and Quattrocento Sans for body

--- config/head.js
+++ config/head.js
@@ -9,7 +9,17 @@ export default {
       content: process.env.npm_package_description || ''
     }
   ],
-  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Ovo'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Quattrocento+Sans'
+    }
+  ],
   script: [
     {
       src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
