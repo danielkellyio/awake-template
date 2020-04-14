7536bbb98120676bbf65b03ec3209bda166184c4
Daniel Kelly <danielkelly@randallreilly.com>
2020-04-13 20:58:48 -0500

    fixed admin path for local dev

--- cms/netlify/build/copy-static-to-dist.js
+++ cms/netlify/build/copy-static-to-dist.js
@@ -3,7 +3,7 @@ const ncp = require('ncp').ncp
 ncp.limit = 16
 
 export default () => {
-  ncp(`${__dirname}/../static`, `${__dirname}/../../../dist`, (err) => {
+  ncp(`${__dirname}/../static`, `${__dirname}/../../../static`, (err) => {
     if (err) {
       return console.error(err)
     }
--- cms/netlify/hooks/beforeBuild.js
+++ cms/netlify/hooks/beforeBuild.js
@@ -1,7 +1,9 @@
 import generatePostApi from '../build/generate-post-api'
 import generateCategoryApi from '../build/generate-category-api'
+import copyStaticToDist from '../build/copy-static-to-dist'
 
 export default () => {
   generatePostApi()
   generateCategoryApi()
+  copyStaticToDist()
 }
