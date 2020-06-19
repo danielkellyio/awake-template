7045c5a43d7a6eee1d0d983086fc2914778e3b80
Daniel Kelly <danielkelly@randallreilly.com>
2020-06-19 12:13:43 -0500

    changed searchFields to array

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,10 +37,10 @@ export default {
         {
           hid: 'og:image',
           property: 'og:image',
-          content: this.$store.state.featureImage 
-          ? (process.env.URL ? process.env.URL : '') +
-            require(`~/assets${this.$store.state.featureImage}`)
-          : ''
+          content: this.$store.state.featureImage
+            ? (process.env.URL ? process.env.URL : '') +
+              require(`~/assets${this.$store.state.featureImage}`)
+            : ''
         },
         {
           hid: 'og:url',
