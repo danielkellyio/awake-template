b7d40bc58c4bf61e04dd7971ca5a4475b71be289
akihisaSakai <akihisa.sakai@soarhap.com>
2021-04-16 02:21:18 +0900

    bugfix: Quotation marks after every table #60

--- components/Markdown.vue
+++ components/Markdown.vue
@@ -67,7 +67,7 @@ export default {
     },
     wrapTable(html) {
       html = html.replace(/<table/g, `<div class="table-wrapper"><table`)
-      html = html.replace(/<\/table>/g, `</table></div>"`)
+      html = html.replace(/<\/table>/g, `</table></div>`)
       return html
     }
   }
